47a5ada3ef611a3f0b92033a5668295483cbafa9
nikhilkumar110k <nikhil4040k@gmail.com>
2024-09-07 00:25:33 +0530

    working location

--- lib/Homepage.dart
+++ lib/Homepage.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:permission_handler/permission_handler.dart';
 import 'package:sih_spot_sync/components/bottom_bar.dart';
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -12,18 +14,52 @@ class Homepage extends StatefulWidget {
 
 
 class _HomepageState extends State<Homepage> {
+
+
+  PermissionStatus _status = PermissionStatus.denied;
+
+  Future<void> _requestLocationPermission() async {
+    final status = await Permission.locationAlways.request();
+    if (status.isDenied) {
+      AlertDialog alertbox = AlertDialog(
+        title: const Text("Please provide the necessary permissions"),
+        actions:
+        <Widget>[
+          TextButton(onPressed: () async {
+            await openAppSettings();
+          }, child: const Text("OK"),),
+          TextButton(onPressed: () {
+            exit(0);
+          }, child: const Text("Leave the App"))
+        ],
+      );
+
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return alertbox;
+        },
+      );
+    }
+    setState(() {
+      _status = status;
+    });
+  }
+
   List<Map<String, dynamic>> loadedEntries = [];
+
   void initState() {
     super.initState();
     _loadData();
   }
+
   Future<void> _loadData() async {
     final url = Uri.parse('https://team007-dc442.firebaseio.com/userdata.json');
     final response = await http.get(url);
 
     if (response.statusCode == 200) {
       final Map<String, dynamic> data = json.decode(response.body);
-      loadedEntries= data["userID"];
+      loadedEntries = data["userID"];
       setState(() {
 
       });
@@ -31,12 +67,14 @@ class _HomepageState extends State<Homepage> {
       throw Exception('Failed to load diary entries');
     }
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title:
-      Text("Welcome!, ${loadedEntries}"),),
+        appBar: AppBar(title:
+        Text("Welcome!, ${loadedEntries}"),),
         bottomNavigationBar: Bottom_Bar()
     );
   }
+
 }
