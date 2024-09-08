2f76ef7756f94ed1fa403c77d82b1aa20e6abe2d
nikhilkumar110k <nikhil4040k@gmail.com>
2024-09-08 07:24:18 +0530

    working profile and completed homepage

--- lib/Homepage.dart
+++ lib/Homepage.dart
@@ -1,16 +1,17 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
 import 'package:lottie/lottie.dart';
 import 'package:permission_handler/permission_handler.dart';
-import 'package:sih_spot_sync/ProfilePage.dart';
-import 'package:sih_spot_sync/components/bottom_bar.dart';
-import 'package:sih_spot_sync/loginandsigninredirector.dart';
-
+import 'ProfilePage.dart';
+import 'components/bottom_bar.dart';
+import 'loginandsigninredirector.dart';
 import 'components/card.dart';
 
 class Homepage extends StatefulWidget {
@@ -22,6 +23,7 @@ class Homepage extends StatefulWidget {
 
 class _HomepageState extends State<Homepage> with SingleTickerProviderStateMixin {
   late AnimationController _animationController;
+  String employeeId='uud1';
   String _locationMessage = "Fetching location...";
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   final List<String> _items = ['Online', 'Near Me', 'Request', 'Propose'];
@@ -29,65 +31,57 @@ class _HomepageState extends State<Homepage> with SingleTickerProviderStateMixin
   int _currentIndex = 1;
   bool _isValueTrue = false;
   late PermissionStatus _status;
-  String loadedEntries='User';
-
+  String loadedEntries = 'User';
   Timer? _timer;
-
-  void startSendingLocation() {
-    _timer = Timer.periodic(const Duration(minutes: 10), (timer) async {
-      await _sendCurrentLocation();
-    });
-  }
-  Future<void> _sendCurrentLocation() async {
-    Position position;
-    try {
-      position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-      const String employeeId = "uud2";
-      await sendLocationToBackend(position.latitude, position.longitude, employeeId);
-    } catch (e) {
-      print('Error fetching location: $e');
-    }
-  }
-
-  void stopSendingLocation() {
-    Future<void> stopLocationUpdates() async {
-    try {
-    bool isServiceRunning = await Geolocator.isLocationServiceEnabled();
-    if (isServiceRunning) {
-    Geolocator.getPositionStream().listen(null).cancel();
-    print("Location updates stopped successfully.");
-    }
-    } catch (e) {
-    print('Error stopping location updates: $e');
-    }
-    }
-  }
-
+  StreamSubscription<Position>? _positionStreamSubscription;
+  Position? _currentPosition;
 
   @override
   void initState() {
     super.initState();
+    _loadData();
+    _sendCurrentLocation();
     _requestLocationPermission();
     _animationController = AnimationController(
       vsync: this,
       duration: const Duration(seconds: 1),
     );
     _animationController.repeat();
-    _loadData();
+    startSendingLocation();
   }
 
   @override
   void dispose() {
     _animationController.dispose();
     _positionStreamSubscription?.cancel();
-
+    stopSendingLocation();
     super.dispose();
   }
 
-  Position? _currentPosition;
-  StreamSubscription<Position>? _positionStreamSubscription;
+  void startSendingLocation() {
+    _timer = Timer.periodic(const Duration(minutes: 10), (timer) async {
+      await _sendCurrentLocation();
+    });
+  }
 
+  void stopSendingLocation() {
+    _timer?.cancel();
+  }
 
+  Future<void> _sendCurrentLocation() async {
+    if (employeeId == null) {
+      print("Employee ID is not set. Cannot send location.");
+      return;
+    }
+
+    Position position;
+    try {
+      position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+      await sendLocationToBackend(position.latitude, position.longitude, employeeId);
+    } catch (e) {
+      print('Error fetching location: $e');
+    }
+  }
 
   Future<void> _requestLocationPermission() async {
     final status = await Permission.locationWhenInUse.request();
@@ -98,12 +92,24 @@ class _HomepageState extends State<Homepage> with SingleTickerProviderStateMixin
       _showPermissionDialog();
     }
   }
+
+  void _startLocationStream() {
+    _positionStreamSubscription = Geolocator.getPositionStream(
+      locationSettings: const LocationSettings(
+        accuracy: LocationAccuracy.high,
+        distanceFilter: 10,
+      ),
+    ).listen((Position position) {
+      setState(() {
+        _currentPosition = position;
+        _locationMessage = 'Lat: ${position.latitude}, Long: ${position.longitude}';
+      });
+    });
+  }
+
   Future<void> sendLocationToBackend(
-      double latitude,
-      double longitude,
-      String employeeId
-      ) async {
-    final String timestamp = DateTime.now().toUtc().toIso8601String();
+      double latitude, double longitude, String employeeId) async {
+    final String timestamp = DateTime.now().toLocal().toIso8601String();
     final url = Uri.parse('https://spotsync.tg-tool.tech/api/store');
 
     final Map<String, dynamic> locationData = {
@@ -121,10 +127,10 @@ class _HomepageState extends State<Homepage> with SingleTickerProviderStateMixin
         headers: {'Content-Type': 'application/json'},
         body: json.encode(locationData),
       );
-
       if (response.statusCode == 200) {
+
         setState(() {
-          _isValueTrue=true;
+          _isValueTrue = true;
         });
         print('Location sent successfully!');
       } else {
@@ -134,6 +140,42 @@ class _HomepageState extends State<Homepage> with SingleTickerProviderStateMixin
       print("Error sending location: $error");
     }
   }
+
+  Future<String?> _loadData() async {
+    try {
+      User? currentUser = FirebaseAuth.instance.currentUser;
+      if (currentUser == null || currentUser.email == null) {
+        print("No user is currently signed in or email is missing.");
+        return null;
+      }
+
+      print("Current User Email: ${currentUser.email}");
+
+      DatabaseReference databaseRef = FirebaseDatabase.instance.ref();
+      DataSnapshot snapshot = await databaseRef.child('userdata').get();
+
+      if (snapshot.exists && snapshot.value is Map) {
+        Map<dynamic, dynamic> allUsers = snapshot.value as Map<dynamic, dynamic>;
+
+        for (var userData in allUsers.values) {
+          if (userData is Map && userData['email'] == currentUser.email) {
+            String? employeeId = userData['employeeidfrombackend'];
+            print("Employee ID found: $employeeId");
+            return employeeId;
+          }
+        }
+
+        print("No matching user data found for email: ${currentUser.email}");
+      } else {
+        print("No user data found in the database.");
+      }
+    } catch (e) {
+      print("Error fetching employee ID: $e");
+    }
+    return null;
+  }
+
+
 
   void _showPermissionDialog() {
     showDialog(
@@ -155,38 +197,6 @@ class _HomepageState extends State<Homepage> with SingleTickerProviderStateMixin
     );
   }
 
-  void _startLocationStream() {
-    _positionStreamSubscription = Geolocator.getPositionStream(
-      locationSettings: const LocationSettings(
-        accuracy: LocationAccuracy.high,
-        distanceFilter: 10,
-      ),
-    ).listen((Position position) {
-      setState(() {
-        _currentPosition = position;
-        _locationMessage = 'Lat: ${position.latitude}, Long: ${position.longitude}';
-      });
-    });
-  }
-
-
-
-  Future<void> _loadData() async {
-    final url = Uri.parse('https://team007-dc442.firebaseio.com/userdata.json');
-    final response = await http.get(url);
-
-    if (response.statusCode == 200) {
-      final Map<String, dynamic> data = json.decode(response.body);
-      print(loadedEntries);
-
-      setState(() {
-        loadedEntries = data["employeeidfrombackend"] ?? [];
-      });
-    } else {
-      throw Exception('Failed to load user data');
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -199,38 +209,38 @@ class _HomepageState extends State<Homepage> with SingleTickerProviderStateMixin
             _scaffoldKey.currentState?.openDrawer();
           },
         ),
-        title: Text("Welcome!, ${loadedEntries.isNotEmpty ? loadedEntries+(', ') : 'User'}"),
+        title: Text("Welcome!, ${loadedEntries.isNotEmpty ? loadedEntries : 'User'}"),
       ),
       drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
           children: <Widget>[
             DrawerHeader(
-              decoration: BoxDecoration(
+              decoration: const BoxDecoration(
                 color: Colors.redAccent,
               ),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                const Text(
-                  "Welcome!,User",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 24,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 20.0),
-                  child: const Text(
-                    "How is the Day!",
+                children: const [
+                  Text(
+                    "Welcome!, User",
                     style: TextStyle(
                       color: Colors.white,
                       fontSize: 24,
                     ),
                   ),
-                ),
-              ],)
+                  Padding(
+                    padding: EdgeInsets.only(top: 20.0),
+                    child: Text(
+                      "How is the Day!",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
             ListTile(
               leading: const Icon(Icons.home),
@@ -243,7 +253,8 @@ class _HomepageState extends State<Homepage> with SingleTickerProviderStateMixin
               leading: const Icon(Icons.person),
               title: const Text('Profile'),
               onTap: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context)=>ProfileEdit1()));
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => const ProfileEdit1()));
               },
             ),
             ListTile(
@@ -257,119 +268,120 @@ class _HomepageState extends State<Homepage> with SingleTickerProviderStateMixin
               leading: const Icon(Icons.logout),
               title: const Text('Logout'),
               onTap: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context)=>LoginandSignPage()));
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => const LoginandSignPage()));
               },
             ),
           ],
         ),
       ),
-      bottomNavigationBar: Bottom_Bar(),
+      bottomNavigationBar:  Bottom_Bar(),
       body: SingleChildScrollView(
         child: Container(
           color: Colors.white,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Column(
+              Center(
+                child: _isValueTrue
+                    ? Lottie.asset(
+                  'images/0lodmNbkGD.json',
+                  controller: _animationController,
+                  height: 400,
+                  width: 400,
+                  fit: BoxFit.fill,
+                )
+                    : Lottie.asset(
+                    'images/Animation - 1725657761664 (1).json',
+                    controller: _animationController,
+                    height: 400,
+                    width: 400,
+                    fit: BoxFit.fill),
+              ),
+              Center(
+                child: Text(
+                  _isValueTrue ? "Active" : "Inactive",
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+              ),
+              Center(
+                child: ElevatedButton(
+                    onPressed: () {
+                      _loadData();
+                      _sendCurrentLocation();
+                      setState(() {
+                        _isValueTrue = !_isValueTrue;
+                      });
+                    },
+                    child: const Text("Change the colour")),
+              ),
+              _isValueTrue
+                  ? Column(
                 children: [
-                  Center(
-                    child: _isValueTrue == true
-                        ? Lottie.asset(
-                      'images/0lodmNbkGD.json',
-                      controller: _animationController,
-                      height: 400,
-                      width: 400,
-                      fit: BoxFit.fill,
-                    )
-                        : Lottie.asset(
-                        'images/Animation - 1725657761664 (1).json',
-                        controller: _animationController,
-                        height: 400,
-                        width: 400,
-                        fit: BoxFit.fill),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 5.0),
+                        child: CardWidget(
+                            color: Color.fromRGBO(80, 193, 84, 100),
+                            heading: "Report",
+                            subText: "See Report"),
+                      ),
+                      CardWidget(
+                          color: Color.fromRGBO(80, 193, 84, 100),
+                          heading: "Leave",
+                          subText: "Take Leave"),
+                    ],
                   ),
-                  Center(
-                      child:
-                      Text(_isValueTrue == true ? "Active" : "Inactive",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),)),
-                  ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          _isValueTrue = !_isValueTrue;
-                        });
-                      },
-                      child: Text("Change the colour")),
-                  SizedBox(
-                    child: _isValueTrue == true
-                        ? Column(
-                      children: [
-                        Row(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(left: 5.0),
-                              child: CardWidget(
-                                  color:
-                                  Color.fromRGBO(80, 193, 84, 100),
-                                  heading: "Report",
-                                  subText: "See Report"),
-                            ),
-                            CardWidget(
-                                color: Color.fromRGBO(80, 193, 84, 100),
-                                heading: "Leave",
-                                subText: "Take Leave"),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(left: 5.0),
-                              child: CardWidget(
-                                  color:
-                                  Color.fromRGBO(80, 193, 84, 100),
-                                  heading: "Tasks",
-                                  subText: "Tasks"),
-                            ),
-                            CardWidget(
-                                color: Color.fromRGBO(80, 193, 84, 100),
-                                heading: "About us",
-                                subText: "Details"),
-                          ],
-                        ),
-                      ],
-                    )
-                        : Column(
-                      children: [
-                        Row(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(left: 5.0),
-                              child: CardWidget(
-                                  color: Color.fromRGBO(255, 142, 58, 100),
-                                  heading: "Report",
-                                  subText: "See Report"),
-                            ),
-                            CardWidget(
-                                color: Color.fromRGBO(255, 142, 58, 100),
-                                heading: "Leave",
-                                subText: "Take Leave"),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(left: 5.0),
-                              child: CardWidget(
-                                  color: Color.fromRGBO(255, 142, 58, 100),
-                                  heading: "Tasks",
-                                  subText: "Tasks"),
-                            ),
-                            CardWidget(
-                                color: Color.fromRGBO(255, 142, 58, 100),
-                                heading: "About us",
-                                subText: "Details"),
-                          ],
-                        ),
-                      ],
-                    ),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 5.0),
+                        child: CardWidget(
+                            color: Color.fromRGBO(80, 193, 84, 100),
+                            heading: "Tasks",
+                            subText: "Tasks"),
+                      ),
+                      CardWidget(
+                          color: Color.fromRGBO(80, 193, 84, 100),
+                          heading: "About us",
+                          subText: "Details"),
+                    ],
+                  ),
+                ],
+              )
+                  : Column(
+                children: [
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 5.0),
+                        child: CardWidget(
+                            color: Color.fromRGBO(255, 142, 58, 100),
+                            heading: "Report",
+                            subText: "See Report"),
+                      ),
+                      CardWidget(
+                          color: Color.fromRGBO(255, 142, 58, 100),
+                          heading: "Leave",
+                          subText: "Take Leave"),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 5.0),
+                        child: CardWidget(
+                            color: Color.fromRGBO(255, 142, 58, 100),
+                            heading: "Tasks",
+                            subText: "Tasks"),
+                      ),
+                       CardWidget(
+                          color: Color.fromRGBO(255, 142, 58, 100),
+                          heading: "About us",
+                          subText: "Details"),
+                    ],
                   ),
                 ],
               ),
